3d0aabcf0d3d5d10385fb6ba6d69b7e4f7cb35e0
bpuchala <bpuchala@users.noreply.github.com>
2020-08-26 17:25:12 -0400

    check if file or directory is included in file selection, and why, locally

--- materials_commons/cli/file_functions.py
+++ materials_commons/cli/file_functions.py
@@ -1,3 +1,4 @@
+import json
 import os.path
 import requests
 import materials_commons.api as mcapi
@@ -48,3 +49,53 @@ def get_by_path_if_exists(client, project_id, file_path):
         if e.response.status_code == 404:
             return None
         raise e
+
+def _check_file_selection_dirs(path, file_selection, orig_path=None):
+    """Recursively checks if a path is included in a dataset file selection, and why"""
+    if path == orig_path:
+        selected_by = "(self)"
+    else:
+        selected_by = path
+    if path in file_selection['include_dirs']:
+        return (True, selected_by)
+    elif path in file_selection['exclude_dirs']:
+        return (False, selected_by)
+    else:
+        parent = os.path.dirname(path)
+        if parent == "/":
+            return (False, None)
+        else:
+            return _check_file_selection_dirs(parent, file_selection, orig_path=orig_path)
+
+def check_file_selection(path, file_selection):
+    """Check if a file or directory is selected in a dataset file selection, and why.
+
+    Notes:
+        A file is selected in a dataset's file selection if it is listed explicitly in "include_files". It is not selected if it is listed in "exclude_files". If it is neither explicitly included or excluded, its parent directories are searched up to the project root directory to see if they are included or excluded. If the first parent directory listed is in "include_dirs", then the file is also selected. If the first parent directory listed is in "exclude_dirs", or if no parent directory is listed at all, the file is not selected.
+
+    Args:
+        path (str): Materials Commons path of file or directory to check
+        file_selection (dict): A file selection dict. Expected format: ::
+
+            {
+                "include_files": [... list of file paths ...],
+                "exclude_files": [... list of file paths ...],
+                "include_dirs": [... list of directory paths ...],
+                "exclude_dirs": [... list of directory paths ...]
+            }
+
+    Returns:
+        (selected, selected_by):
+
+        selected (bool): True if selected, False if not selected
+
+        selected_by (str or None): One of "(self)" if included/excluded explicitly; Else, "<path>", the path of the first parent directory that is included or excluded; Otherwise, None, to indicate that it is not selected, but neither included nor excluded.
+    """
+    result = None
+    if path in file_selection['include_files']:
+        result = (True, "(self)")
+    elif path in file_selection['exclude_files']:
+        result = (False, "(self)")
+    else:
+        result = _check_file_selection_dirs(path, file_selection, orig_path=path)
+    return result
