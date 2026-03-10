dbccf743454c0156ef15fe7a588fa6fe3fd1097a
gtarcea <gtarcea@umich.edu>
2026-03-10 17:37:38 -0400

    Incorporate parallel downloads into down command

--- materials_commons/cli/subcommands/down.py
+++ materials_commons/cli/subcommands/down.py
@@ -1,4 +1,5 @@
 import argparse
+import asyncio
 import io
 import os
 import requests
@@ -11,10 +12,13 @@ import materials_commons.cli.functions as clifuncs
 import materials_commons.cli.globus as cliglobus
 import materials_commons.cli.tree_functions as treefuncs
 import materials_commons.cli.file_functions as filefuncs
+from materials_commons.cli.server.downloader.file_download_manager import FileDownloadManager
 from materials_commons.cli.treedb import LocalTree, RemoteTree
+from materials_commons.cli.user_config import Config
+
 
 def _get_current_globus_download(pconfig, proj, verbose=True):
-    all_downloads = {download.id:download for download in proj.remote.get_all_globus_download_requests(proj.id)}
+    all_downloads = {download.id: download for download in proj.remote.get_all_globus_download_requests(proj.id)}
 
     globus_download_id = None
     if pconfig.globus_download_id:
@@ -37,8 +41,8 @@ def _get_current_globus_download(pconfig, proj, verbose=True):
 
     return download
 
-def _check_download_file(proj_id, file_id, local_path, remote, working_dir,
-                         force=False):
+
+def _check_download_file(proj_id, file_id, local_path, remote, working_dir, force=False):
     """Prompt user for confirmation before overwriting an existing local file
 
     Arguments
@@ -75,7 +79,9 @@ def _check_download_file(proj_id, file_id, local_path, remote, working_dir,
                 break
     return None
 
-def standard_download(proj, path, working_dir, force=False, output=None, recursive=False, no_compare=False, localtree=None, remotetree=None):
+
+def standard_download(proj, path, working_dir, force=False, output=None, recursive=False, no_compare=False,
+                      localtree=None, remotetree=None):
     """Download files and directories
 
     Arguments
@@ -137,10 +143,10 @@ def standard_download(proj, path, working_dir, force=False, output=None, recursi
             return True
         else:
             try:
-                result_path =  _check_download_file(proj.id,
-                                                    files_data[path]['id'],
-                                                    output, proj.remote,
-                                                    working_dir, force=force)
+                result_path = _check_download_file(proj.id,
+                                                   files_data[path]['id'],
+                                                   output, proj.remote,
+                                                   working_dir, force=force)
             except Exception as e:
                 print(printpath + ": " + str(e) + " (skipping)")
                 return False
@@ -180,6 +186,156 @@ def standard_download(proj, path, working_dir, force=False, output=None, recursi
         print(printpath + ": does not exist on remote")
         return False
 
+
+async def standard_download_async(proj, path, working_dir, force=False, output=None, recursive=False, no_compare=False,
+                                  localtree=None, remotetree=None, file_download_manager=None):
+    """Download files and directories asynchronously
+
+    Arguments
+    ---------
+    proj: mcapi.Project, Project to download from
+
+    path: str, Materials Commons style path of file or directory to download
+
+    working_dir (str): Current working directory, used for finding relative
+        paths and printing messages.
+
+    force: bool (optional, default=False) If True, force overwrite existing files without confirmation.
+
+    output: str (optional, default=None)
+        Specify a different download location. By default, files are downloaded to the matching
+        location in the local project directory. For example remote file at "/A/B/file.txt" is
+        downloaded to "<proj.local_path>/A/B/file.txt" by default.
+
+    recursive: bool (optional, default=False) Download directory contents recursively.
+
+    no_compare: bool (optional, default=False)
+        By default, this function checks local and remote file checksum to avoid downloading files
+        that already exist. If no_compare is True, this check is skipped and all specified files are
+        downloaded, even if an equivalent file already exists locally.
+
+    localtree: LocalTree object (optional, default=None)
+        A LocalTree object stores local file checksums to avoid unnecessary hashing. Will be used
+        and updated if provided and checksum == True.
+
+    remotetree: RemoteTree object (optional, default=None)
+        A RemoteTree object stores remote file and directory information to minimize API calls and
+        data transfer. Will be used and updated if provided.
+
+    Returns
+    -------
+    success: bool, True if download succeeds, False otherwise
+    """
+    local_abspath = filefuncs.make_local_abspath(proj.local_path, path)
+    printpath = os.path.relpath(local_abspath, start=working_dir)
+
+    if output is None:
+        output = local_abspath
+
+    checksum = True
+    if no_compare:
+        checksum = False
+
+    files_data, dirs_data, child_data, non_existing = treefuncs.treecompare(
+        proj, [path], checksum=checksum, localtree=localtree, remotetree=remotetree)
+
+    # if remote file:
+    if path in files_data and files_data[path]['r_type'] == 'file':
+
+        if files_data[path]['l_type'] == 'directory':
+            print(printpath + ": is local directory and remote file")
+            return False
+        elif 'eq' in files_data[path] and files_data[path]['eq'] and output == local_abspath:
+            print(printpath + ": local is equivalent to remote (skipping)")
+            return True
+        else:
+            try:
+                result_path = await _check_download_file_async(proj.id,
+                                                               files_data[path]['id'],
+                                                               output, proj.remote,
+                                                               working_dir,
+                                                               file_download_manager=file_download_manager,
+                                                               force=force)
+            except Exception as e:
+                print(printpath + ": " + str(e) + " (skipping)")
+                return False
+            if result_path:
+                if output != local_abspath:
+                    print("downloaded:", printpath, "as",
+                          os.path.relpath(output, start=working_dir))
+                else:
+                    print("downloaded:", printpath)
+                return True
+            else:
+                return False
+
+    # if directory:
+    elif path in dirs_data and dirs_data[path]['r_type'] == 'directory':
+
+        if not recursive:
+            print(printpath + ": is a directory")
+            return False
+
+        if dirs_data[path]['l_type'] == 'file':
+            print(printpath + ": is local file and remote directory")
+            return False
+
+        success = True
+        for childpath, record in child_data[path].items():
+            childoutput = os.path.join(output, os.path.basename(childpath))
+            success &= await standard_download_async(proj, childpath, working_dir,
+                                                     force=force, output=childoutput,
+                                                     recursive=recursive,
+                                                     no_compare=no_compare,
+                                                     localtree=localtree,
+                                                     remotetree=remotetree,
+                                                     file_download_manager=file_download_manager)
+        return success
+
+    else:
+        print(printpath + ": does not exist on remote")
+        return False
+
+
+async def _check_download_file_async(proj_id, file_id, local_path, remote, working_dir,
+                                     file_download_manager, force=False):
+    """Prompt user for confirmation before overwriting an existing local file
+
+    Arguments
+    ---------
+    proj_id: int, Project ID
+    file_id: int, ID of file to download
+    local_path: str, Location to download file. Intermediate directories are created if necessary.
+    remote: mcapi.Client, Materials Commons Client
+    working_dir (str): Current working directory, used for finding relative
+        paths and printing messages.
+    force: bool (optional, default=False) If True, force overwrite existing file without confirmation.
+
+    Returns
+    -------
+    local_path: str or None, Location of downloaded file or None if not downloaded
+    """
+    if not os.path.exists(local_path) or force:
+        dir = os.path.dirname(local_path)
+        if not os.path.isdir(dir):
+            os.makedirs(dir)
+        await file_download_manager.download_file(proj_id, file_id, local_path)
+        return local_path
+    else:
+        print("Overwrite '" + os.path.relpath(local_path, working_dir) + "'?")
+        while True:
+            ans = input('y/n: ')
+            if ans == 'y':
+                dir = os.path.dirname(local_path)
+                if not os.path.exists(dir):
+                    os.makedirs(dir)
+                await file_download_manager.download_file(proj_id, file_id, local_path)
+                return local_path
+            elif ans == 'n':
+                break
+    return None
+
+
 def download_file_as_string(client, project_id, file_id):
     urlpart = "/projects/" + str(project_id) + "/files/" + str(file_id) + "/download"
     url = client.base_url + urlpart
@@ -189,6 +345,7 @@ def download_file_as_string(client, project_id, file_id):
         for block in r.iter_content(chunk_size=8192):
             f.write(block)
         return f.getvalue().decode('utf-8')
+
 
 def print_file(proj, path, working_dir):
     """Print a remote file, without writing it locally
@@ -217,6 +374,7 @@ def print_file(proj, path, working_dir):
     print(printpath + ":")
     print(s, end='')
 
+
 def make_parser():
     """Make argparse.ArgumentParser for `mc down`"""
 
@@ -234,6 +392,8 @@ def make_parser():
     parser.add_argument('paths', nargs='*', default=None, help='Files or directories')
     parser.add_argument('-r', '--recursive', action="store_true", default=False,
                         help='Download directory contents recursively')
+    parser.add_argument('--parallel', action="store_true", default=False,
+                        help='Download files in parallel (not supported with Globus)')
     parser.add_argument('-f', '--force', action="store_true", default=False,
                         help='Force overwrite of existing files')
     parser.add_argument('-p', '--print', action="store_true", default=False,
@@ -246,6 +406,7 @@ def make_parser():
     parser.add_argument('--no-compare', action="store_true", default=False,
                         help='Download remote without checking if local is equivalent.')
     return parser
+
 
 def down_subcommand(argv, working_dir):
     """
@@ -284,7 +445,7 @@ def down_subcommand(argv, working_dir):
     if args.globus:
         download = _get_current_globus_download(pconfig, proj)
 
-        if download.status != 0:    # TODO clean up status code / message
+        if download.status != 0:  # TODO clean up status code / message
 
             print("Checking if download is ready.", end='', flush=True)
             count = 0
@@ -297,8 +458,8 @@ def down_subcommand(argv, working_dir):
 
         if download.status != 0:
             print("Current Globus download (name=" + download.name + ", id=" + str(download.id) + ")"
-                + " not ready for downloading. Materials Commons is still preparing the project"
-                + " files for download. For large projects this may take some time.")
+                  + " not ready for downloading. Materials Commons is still preparing the project"
+                  + " files for download. For large projects this may take some time.")
             print("Use `mc globus download` to check when it is ready and try again.")
             return
 
@@ -320,21 +481,55 @@ def down_subcommand(argv, working_dir):
             print("Use `mc globus download` to manage Globus downloads.")
             print("Multiple transfer tasks may be initiated.")
             print("When all tasks finish downloading, use `mc globus download --id " + str(download.id) +
-                " --delete` " + "to close the download.")
+                  " --delete` " + "to close the download.")
 
     elif args.print:
         print_file(proj, paths[0], working_dir)
 
     else:
-
-        output=None
+        output = None
         if args.output:
             output = os.path.abspath(args.output[0])
 
-        for path in paths:
-            standard_download(proj, path, working_dir, force=args.force,
-                              output=output, recursive=args.recursive,
-                              no_compare=args.no_compare, localtree=localtree,
-                              remotetree=remotetree)
+        if args.parallel:
+            asyncio.run(download_async(proj, paths, working_dir, force=args.force,
+                                       output=output, recursive=args.recursive,
+                                       no_compare=args.no_compare, localtree=localtree,
+                                       remotetree=remotetree))
+        else:
+            for path in paths:
+                standard_download(proj, path, working_dir, force=args.force,
+                                  output=output, recursive=args.recursive,
+                                  no_compare=args.no_compare, localtree=localtree,
+                                  remotetree=remotetree)
 
     return
+
+
+async def download_async(proj, paths, working_dir, force=False, output=None, recursive=False, no_compare=False,
+                         localtree=None, remotetree=None):
+    """Download files and directories in parallel"""
+    print("Downloading files in parallel...")
+    config = Config()
+    queue = asyncio.Queue()
+    file_download_manager = FileDownloadManager(send_queue=queue,
+                                                client_id=config.client_uuid,
+                                                mcurl=config.default_remote.mcurl,
+                                                apitoken=config.default_remote.mcapikey,
+                                                max_concurrent=3)
+    download_workers = await file_download_manager.start_workers()
+
+    try:
+        for path in paths:
+            await standard_download_async(proj, path, working_dir, force=force,
+                                          output=output, recursive=recursive,
+                                          no_compare=no_compare, localtree=localtree,
+                                          remotetree=remotetree, file_download_manager=file_download_manager)
+        await file_download_manager.wait_all()
+    finally:
+        file_download_manager.stop_workers()
+        for worker in download_workers:
+            try:
+                await worker
+            except asyncio.CancelledError:
+                pass
