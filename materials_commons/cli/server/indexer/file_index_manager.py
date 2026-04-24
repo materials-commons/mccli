0f77329ac2cf575afb2e4cde07ba348f48fa2680
gtarcea <gtarcea@umich.edu>
2026-04-24 14:30:46 -0400

    Similar to the DBManager changes, now send a structured request with the project and db

--- materials_commons/cli/server/indexer/file_index_manager.py
+++ materials_commons/cli/server/indexer/file_index_manager.py
@@ -1,13 +1,15 @@
 import asyncio
 import os
-from _asyncio import Task
+from dataclasses import dataclass
+from pathlib import Path
 from typing import Optional
 
 from materials_commons.api import models
 
+from materials_commons.cli.local_project import LocalProject
 from materials_commons.cli.models import FileRecord
 from materials_commons.cli.reconcile2 import observe_and_reconcile
-from materials_commons.cli.server.project_filedbs import ProjectFileDBs
+from materials_commons.cli.server.db.db_manager import DBWriteRequest
 
 
 def file_has_changed(file_record: FileRecord, finfo: os.stat_result) -> bool:
@@ -20,64 +22,71 @@ def file_has_changed(file_record: FileRecord, finfo: os.stat_result) -> bool:
             file_record.local_mtime_ns != finfo.st_mtime_ns
     )
 
+@dataclass(frozen=True)
+class IndexRequest:
+    """Request to index a file"""
+    file_path: str | Path
+    project_path: str | Path
+    remote_entry: Optional[models.File]
+    project: LocalProject
 
 class FileIndexManager:
     """Manages multiple concurrent file indexers"""
 
-    def __init__(self, project_file_dbs: ProjectFileDBs, db_queue: asyncio.Queue, index_queue: asyncio.Queue,
+    def __init__(self, db_queue: asyncio.Queue, index_queue: asyncio.Queue[IndexRequest],
                  max_concurrent: int = 3):
-        self._project_file_dbs = project_file_dbs
         self._db_queue = db_queue
-        self._index_queue = index_queue
+        self._index_queue: asyncio.Queue[IndexRequest] = index_queue
         self._workers_running = False
         self._max_concurrent = max_concurrent
+        self._worker_tasks: list[asyncio.Task] = []
 
-    async def start_workers(self) -> list[Task[None]]:
+    async def start_workers(self) -> None:
         """Start background workers to process the index queue"""
         self._workers_running = True
-        workers = [
+        self._worker_tasks = [
             asyncio.create_task(self._index_file_worker())
             for i in range(self._max_concurrent)
         ]
-        return workers
 
     async def _index_file_worker(self):
-        """Worker that processes index requests from queue"""
+        """Worker that processes index requests from the indexer queue"""
         while self._workers_running:
             try:
-                (file_path, project_path, remote_entry, project_id) = await asyncio.wait_for(self._index_queue.get(),
-                                                                                             timeout=1.0)
+                index_request: IndexRequest = await asyncio.wait_for(self._index_queue.get(), timeout=1.0)
             except asyncio.TimeoutError:
                 continue
 
             try:
-                await self._index(file_path, project_path, remote_entry, project_id)
+                await self._index(index_request)
             except Exception as e:
-                print(f"Error indexing {file_path}: {e}")
-                raise e
+                print(f"Error indexing {index_request.file_path}: {e}")
             finally:
                 self._index_queue.task_done()
 
-    async def _index(self, file_path: str, project_path: str, remote_entry: Optional[models.File], project_id: int):
+    async def _index(self, index_request: IndexRequest):
         """Index a file. Checks if the file has changed since the last index."""
-        db = await self._project_file_dbs.get_filedb(project_id)
-        if db is None:
-            raise Exception(
-                f"Project {project_id} not found in filedb."
-            )
+        db = await index_request.project.get_filedb()
         file_decision = await observe_and_reconcile(db=db,
-                                                    project_path=project_path,
-                                                    file_path=file_path,
-                                                    remote_entry=remote_entry,
+                                                    project_path=Path(index_request.project_path).as_posix(),
+                                                    file_path=Path(index_request.file_path).as_posix(),
+                                                    remote_entry=index_request.remote_entry,
                                                     recompute_checksum=True)
 
         if not file_decision.updated_record:
-            print(f"Skipping {file_path} in {project_path} as it hasn't changed")
+            print(f"Skipping {index_request.file_path} in {index_request.project_path} as it hasn't changed")
             return
 
-        await self._db_queue.put(("single", project_id, file_decision.updated_record))
-        print(f"Indexed {file_path} in {project_path}")
+        db_write_request = DBWriteRequest(project=index_request.project, command="single", data=file_decision.updated_record)
+        await self._db_queue.put(db_write_request)
+        print(f"Indexed {index_request.file_path} in {index_request.project_path}", flush=True)
 
-    def stop_workers(self):
+    async def stop_workers(self):
         """Stop background workers"""
         self._workers_running = False
+        for worker in self._worker_tasks:
+            worker.cancel()
+            try:
+                await worker
+            except asyncio.CancelledError:
+                pass
