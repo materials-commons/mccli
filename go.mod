af00da21b202fde8a49ede851ad3298b8594fded
gtarcea <gtarcea@umich.edu>
2026-09-16 16:24:58 -0400

    WIP - Setup command.

--- go.mod
+++ go.mod
@@ -16,10 +16,24 @@ require (
 )
 
 require (
+	charm.land/bubbles/v2 v2.0.0 // indirect
+	charm.land/bubbletea/v2 v2.0.2 // indirect
+	charm.land/huh/v2 v2.0.3 // indirect
+	charm.land/lipgloss/v2 v2.0.1 // indirect
 	github.com/BurntSushi/toml v1.6.0 // indirect
 	github.com/VividCortex/ewma v1.2.0 // indirect
 	github.com/acarl005/stripansi v0.0.0-20180116102854-5a71ef0e047d // indirect
+	github.com/atotto/clipboard v0.1.4 // indirect
+	github.com/catppuccin/go v0.2.0 // indirect
 	github.com/cespare/xxhash/v2 v2.3.0 // indirect
+	github.com/charmbracelet/colorprofile v0.4.2 // indirect
+	github.com/charmbracelet/ultraviolet v0.0.0-20260205113103-524a6607adb8 // indirect
+	github.com/charmbracelet/x/ansi v0.11.6 // indirect
+	github.com/charmbracelet/x/exp/ordered v0.1.0 // indirect
+	github.com/charmbracelet/x/exp/strings v0.0.0-20240722160745-212f7b056ed0 // indirect
+	github.com/charmbracelet/x/term v0.2.2 // indirect
+	github.com/charmbracelet/x/termios v0.1.1 // indirect
+	github.com/charmbracelet/x/windows v0.2.2 // indirect
 	github.com/clipperhouse/displaywidth v0.11.0 // indirect
 	github.com/clipperhouse/uax29/v2 v2.7.0 // indirect
 	github.com/dmotylev/goproperties v0.0.0-20140630191356-7cbffbaada47 // indirect
@@ -31,19 +45,25 @@ require (
 	github.com/gosimple/unidecode v1.0.1 // indirect
 	github.com/jinzhu/inflection v1.0.0 // indirect
 	github.com/jinzhu/now v1.1.5 // indirect
+	github.com/lucasb-eyer/go-colorful v1.3.0 // indirect
 	github.com/materials-commons/config v0.0.0-20180218183642-ed5747ab2e08 // indirect
 	github.com/mattn/go-colorable v0.1.15 // indirect
 	github.com/mattn/go-isatty v0.0.24 // indirect
 	github.com/mattn/go-runewidth v0.0.29 // indirect
 	github.com/mattn/go-sqlite3 v1.14.49 // indirect
+	github.com/mitchellh/hashstructure/v2 v2.0.2 // indirect
+	github.com/muesli/cancelreader v0.2.2 // indirect
 	github.com/ncruces/go-strftime v1.0.0 // indirect
 	github.com/olekukonko/cat v0.0.0-20250911104152-50322a0618f6 // indirect
 	github.com/olekukonko/errors v1.3.0 // indirect
 	github.com/olekukonko/ll v0.1.8 // indirect
 	github.com/remyoudompheng/bigfft v0.0.0-20230129092748-24d4a6f8daec // indirect
+	github.com/rivo/uniseg v0.4.7 // indirect
 	github.com/spf13/cast v1.10.0 // indirect
 	github.com/vbauerster/cupwriter v0.0.4 // indirect
+	github.com/xo/terminfo v0.0.0-20220910002029-abceb7e1c41e // indirect
 	golang.org/x/net v0.59.0 // indirect
+	golang.org/x/sync v0.23.0 // indirect
 	golang.org/x/sys v0.48.0 // indirect
 	golang.org/x/text v0.42.0 // indirect
 	gopkg.in/yaml.v1 v1.0.0-20140924161607-9f9df34309c0 // indirect
--- pkg/cmds/setup.go
+++ pkg/cmds/setup.go
@@ -1,9 +1,203 @@
 package cmds
 
 import (
+	"os/exec"
+	"strings"
+
+	"charm.land/huh/v2"
 	"github.com/materials-commons/mccli/pkg/config"
+	"github.com/materials-commons/mccli/pkg/di"
 )
 
+type setupRunner struct {
+	deps          di.Dependencies
+	fdCommandPath string
+	rgCommandPath string
+}
+
 func RunSetupCmd(config config.Global, cfgLoadErr error) error {
+	return (&setupRunner{deps: di.Production()}).run(config, cfgLoadErr)
+}
+
+func (r *setupRunner) run(config config.Global, cfgLoadErr error) error {
+	// Show the user the initial setup form. This will prompt them to install
+	// any missing tools.
+	installTools, err := r.runInitialSetupForm()
+	if err != nil {
+		return err
+	}
+
+	if installTools {
+		if err := r.installToolsFunc(); err != nil {
+			return err
+		}
+	}
+
+	// Check if fd and ripgrep are installed. If they are then we can prompt to configure project locations.
+	// Otherwise, just skip this step.
+	if r.fdCommandPath != "" && r.rgCommandPath != "" {
+		configureProjectLocations, err := r.runConfigureProjectLocationsForm()
+		if err != nil {
+			return err
+		}
+
+		var projectLocations []string
+
+		if configureProjectLocations {
+			locations, err := r.promptProjectLocations()
+			if err != nil {
+				return err
+			}
+
+			projectLocations = locations
+		}
+
+		_ = projectLocations
+	}
+
 	return nil
+}
+
+func (r *setupRunner) runInitialSetupForm() (bool, error) {
+	var installTools bool
+
+	// Check to see if we need to install fd and/or ripgrep
+	hasFdFind := r.findFdCommand()
+	hasRg := r.findRgCommand()
+
+	missingTools := !hasFdFind || !hasRg
+
+	welcome := `Welcome to the Materials Commons CLI setup wizard.
+Here you will configure mccli to connect to a Materials Commons server, and setup optional features.
+You can rerun this wizard at any time by running 'mc2 setup'.
+
+You will be taken through the following steps:
+
+1. Setup your auth token so that 'mc2' can connect to a Materials Commons server.
+
+2. If you don't have 'fdfind' and/or 'ripgrep' installed you will be asked if you want to install them.
+
+   This is optional, but the commands are required if you want to use the 'mc2 search' and 'mc2 find' commands.
+   These commands allow you to find files and directories by name (mc2 find) or search file content (mc2 search).
+
+3. If you already have 'fdfind' and/or 'ripgrep' installed or you chose to have 'mc2' install them, you will be given
+   the option to configure where your projects are located on your computer. This is optional, but it is recommended.
+   Doing this will allow you to search and find files across multiple projects from anywhere on your computer.
+`
+
+	form := huh.NewForm(
+		huh.NewGroup(huh.NewNote().
+			Title("Materials Commons CLI Setup").
+			Description(welcome).
+			Next(true).
+			NextLabel("Next")),
+		huh.NewGroup(
+			huh.NewConfirm().
+				Title("Install optional tools?").
+				Description("fdfind/fd and/or ripgrep/rg were not found. Would you like to install them?").
+				Value(&installTools).
+				Affirmative("Install").
+				Negative("Skip"),
+		).WithHideFunc(func() bool {
+			return !missingTools
+		}),
+	)
+
+	if err := form.Run(); err != nil {
+		return false, err
+	}
+
+	if !missingTools {
+		return false, nil
+	}
+
+	return installTools, nil
+}
+
+func (r *setupRunner) runConfigureProjectLocationsForm() (bool, error) {
+	var configureProjectLocations bool
+	form := huh.NewForm(
+		huh.NewGroup(
+			huh.NewConfirm().
+				Title("Configure project locations?").
+				Description("Would you like to configure where your projects are located on your computer?").
+				Value(&configureProjectLocations).
+				Affirmative("Configure").
+				Negative("Skip"),
+		))
+
+	if err := form.Run(); err != nil {
+		return false, err
+	}
+
+	return configureProjectLocations, nil
+}
+
+func (r *setupRunner) promptProjectLocations() ([]string, error) {
+	var locations []string
+
+	for {
+		var location string
+
+		form := huh.NewForm(
+			huh.NewGroup(
+				huh.NewInput().
+					Title("Project location").
+					Description("Enter a directory containing Materials Commons projects, or press Enter when finished.").
+					Placeholder("/path/to/projects").
+					Value(&location),
+			),
+		)
+
+		if err := form.Run(); err != nil {
+			return nil, err
+		}
+
+		location = strings.TrimSpace(location)
+		if location == "" {
+			break
+		}
+
+		locations = append(locations, location)
+	}
+
+	return locations, nil
+}
+
+func (r *setupRunner) installToolsFunc() error {
+	return nil
+}
+
+func (r *setupRunner) findCommand(name string) (string, error) {
+	path, err := exec.LookPath(name)
+	if err != nil {
+		return "", err
+	}
+	return path, nil
+}
+
+func (r *setupRunner) findFdCommand() bool {
+	path, err := r.findCommand("fdfind")
+	if err == nil {
+		r.fdCommandPath = path
+		return true
+	}
+
+	path, err = r.findCommand("fd")
+	if err == nil {
+		r.fdCommandPath = path
+		return true
+	}
+
+	return false
+}
+
+func (r *setupRunner) findRgCommand() bool {
+	path, err := r.findCommand("rg")
+	if err == nil {
+		r.rgCommandPath = path
+		return true
+	}
+
+	return false
 }
